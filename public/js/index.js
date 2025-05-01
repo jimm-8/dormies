91a4108299a422901ea50035af286df626576dd0
jimm-8 <peacti10@gmail.com>
2025-05-01 18:36:25 +0800

    fg

--- public/js/index.js
+++ public/js/index.js
@@ -17,6 +17,22 @@ setTimeout(() => {
   }, 800);
 }, 3000);
 
+function initNavbarEvents() {
+  const menuButton = document.getElementById("acc_btn");
+  if (menuButton) {
+    menuButton.addEventListener("click", function () {
+      location.href = "/landing.html";
+    });
+  }
+
+  const imgBtn = document.getElementById("nav_logo");
+  if (imgBtn) {
+    imgBtn.addEventListener("click", () => {
+      location.href = "/index.html";
+    });
+  }
+}
+
 const owner_btn = document.getElementById("owner_btn");
 const renter_btn = document.getElementById("renter_btn");
 
--- public/js/renter/home.js
+++ public/js/renter/home.js
@@ -34,14 +34,14 @@ function initNavbarEvents() {
   const menuButton = document.getElementById("acc_btn");
   if (menuButton) {
     menuButton.addEventListener("click", function () {
-      location.href = "/landing.html";
+      location.href = "/pages/renter/account.html";
     });
   }
 
   const imgBtn = document.getElementById("nav_logo");
   if (imgBtn) {
     imgBtn.addEventListener("click", () => {
-      location.href = "/index.html";
+      location.href = "/pages/renter/home.html";
     });
   }
 }
